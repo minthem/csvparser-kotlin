b96063531403393c82a027cdfc69138d16774ca3
minthem <114272528+minthem@users.noreply.github.com>
2025-10-03 01:21:00 +0900

    Refactor `ConfigTest` to use nested test structure and add validation tests for `CsvConfig` and `ReaderConfig`

--- src/main/kotlin/io/github/minthem/config/Config.kt
+++ src/main/kotlin/io/github/minthem/config/Config.kt
@@ -8,14 +8,22 @@ data class CsvConfig(
     val locale: Locale = Locale.getDefault(),
     val strictMode: Boolean = true,
     val nullValue: String = "",
-)
+) {
+    init {
+        if (delimiter == quoteChar) throw IllegalArgumentException("delimiter and quoteChar cannot be same.")
+    }
+}
 
 data class ReaderConfig(
     val skipRows: Int = 0,
     val hasHeader: Boolean = true,
     val ignoreBlankLine: Boolean = false,
     val skipInvalidLine: Boolean = false,
-)
+) {
+    init {
+        if (skipRows < 0) throw IllegalArgumentException("skipRows cannot be negative.")
+    }
+}
 
 data class WriterConfig(
     val lineSeparator: LineSeparator = LineSeparator.SYSTEM,
