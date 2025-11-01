69781cf5ee058bb8373ec2be4cb22e5b17fabb89
minthem <114272528+minthem@users.noreply.github.com>
2025-11-01 22:21:40 +0900

    Refactor `CsvEntityReader` and `CsvEntityWriter` to centralize `CsvConverter` initialization using `CsvConverterHelper`, reducing duplication and improving maintainability.

---
+++ src/main/kotlin/io/github/minthem/core/CsvConverterHelper.kt
@@ -0,0 +1,88 @@
+package io.github.minthem.core
+
+import io.github.minthem.annotation.BooleanCsvField
+import io.github.minthem.annotation.CsvFieldFormat
+import io.github.minthem.converter.BigDecimalCsvConverter
+import io.github.minthem.converter.BooleanCsvConverter
+import io.github.minthem.converter.ByteCsvConverter
+import io.github.minthem.converter.CsvConverter
+import io.github.minthem.converter.DoubleCsvConverter
+import io.github.minthem.converter.FloatCsvConverter
+import io.github.minthem.converter.IntCsvConverter
+import io.github.minthem.converter.LocalDateCsvConverter
+import io.github.minthem.converter.LocalDateTimeCsvConverter
+import io.github.minthem.converter.LongCsvConverter
+import io.github.minthem.converter.ShortCsvConverter
+import io.github.minthem.converter.StringCsvConverter
+import io.github.minthem.exception.CsvUnsupportedTypeException
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.Locale
+import kotlin.reflect.KAnnotatedElement
+import kotlin.reflect.KClass
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KType
+import kotlin.reflect.full.findAnnotation
+
+internal object CsvConverterHelper {
+
+    fun resolve(clazz: KClass<*>, field: KParameter): CsvConverter<*> {
+        return buildConverter(clazz, field.type, field, field.name)
+    }
+
+    fun resolve(clazz: KClass<*>, field: KProperty1<*, *>): CsvConverter<*> {
+        return buildConverter(clazz, field.returnType, field, field.name)
+    }
+
+    private fun buildConverter(
+        clazz: KClass<*>,
+        type: KType,
+        field: KAnnotatedElement,
+        fieldName: String?
+    ): CsvConverter<*> {
+        val fieldFmt = field.findAnnotation<CsvFieldFormat>()
+
+        if (type.classifier == String::class) {
+            return StringCsvConverter()
+        }
+        if (type.classifier == Boolean::class) {
+            val boolFmt = field.findAnnotation<BooleanCsvField>() ?: BooleanCsvField()
+            return BooleanCsvConverter
+                .Builder()
+                .trueValues(boolFmt.trueValues.toList())
+                .falseValues(boolFmt.falseValues.toList())
+                .caseSensitive(!boolFmt.ignoreCase)
+                .build()
+        }
+
+        var builder = when (type.classifier) {
+            Int::class -> IntCsvConverter.Builder()
+            Long::class -> LongCsvConverter.Builder()
+            Short::class -> ShortCsvConverter.Builder()
+            Byte::class -> ByteCsvConverter.Builder()
+            Float::class -> FloatCsvConverter.Builder()
+            Double::class -> DoubleCsvConverter.Builder()
+            BigDecimal::class -> BigDecimalCsvConverter.Builder()
+            LocalDate::class -> LocalDateCsvConverter.Builder()
+            LocalDateTime::class -> LocalDateTimeCsvConverter.Builder()
+            else -> {
+                throw CsvUnsupportedTypeException(
+                    clazz,
+                    fieldName,
+                    type,
+                )
+            }
+        }
+
+        fieldFmt?.locale?.let { builder = builder.locale(Locale.forLanguageTag(it)) }
+
+        if (fieldFmt?.pattern.isNullOrBlank().not()) {
+            builder = builder.pattern(fieldFmt.pattern)
+        }
+
+        return builder.build()
+    }
+
+}
--- src/main/kotlin/io/github/minthem/core/CsvEntityWriter.kt
+++ src/main/kotlin/io/github/minthem/core/CsvEntityWriter.kt
@@ -1,29 +1,11 @@
 package io.github.minthem.core
 
-import io.github.minthem.annotation.BooleanCsvField
 import io.github.minthem.annotation.CsvField
-import io.github.minthem.annotation.CsvFieldFormat
 import io.github.minthem.config.CsvConfig
 import io.github.minthem.config.WriterConfig
-import io.github.minthem.converter.BigDecimalCsvConverter
-import io.github.minthem.converter.BooleanCsvConverter
-import io.github.minthem.converter.ByteCsvConverter
 import io.github.minthem.converter.CsvConverter
-import io.github.minthem.converter.DoubleCsvConverter
-import io.github.minthem.converter.FloatCsvConverter
-import io.github.minthem.converter.IntCsvConverter
-import io.github.minthem.converter.LocalDateCsvConverter
-import io.github.minthem.converter.LocalDateTimeCsvConverter
-import io.github.minthem.converter.LongCsvConverter
-import io.github.minthem.converter.ShortCsvConverter
-import io.github.minthem.converter.StringCsvConverter
 import io.github.minthem.exception.CsvEntityMappingException
 import io.github.minthem.exception.CsvFieldConvertException
-import io.github.minthem.exception.CsvUnsupportedTypeException
-import java.math.BigDecimal
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.Locale
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
@@ -65,7 +47,7 @@ class CsvEntityWriter<T : Any>(
     private val writer = CsvWriter(out, config, writeConfig)
     private val formatter = CsvLineFormatter(config.delimiter, config.quoteChar)
 
-    private val valueProperties: List<Cell?> by lazy {
+    private val valueProperties: List<CsvConverterAdapter<*>?> by lazy {
         buildHeaderIndexMap()
     }
 
@@ -117,20 +99,20 @@ class CsvEntityWriter<T : Any>(
         out.append(line).append(writeConfig.lineSeparator.value)
     }
 
-    private fun buildHeaderIndexMap(): List<Cell?> {
+    private fun buildHeaderIndexMap(): List<CsvConverterAdapter<*>?> {
         val ctor = entityClass.primaryConstructor!!
         val params = ctor.parameters
         val props = entityClass.memberProperties.associateBy { it.name }
 
-        val headerIndexMap = mutableMapOf<Int, Cell>()
-        val unIndexProperties = mutableListOf<Cell>()
+        val headerIndexMap = mutableMapOf<Int, CsvConverterAdapter<*>>()
+        val unIndexProperties = mutableListOf<CsvConverterAdapter<*>>()
 
         for (param in params) {
             val prop = props[param.name] ?: continue
             val csvField = prop.findAnnotation<CsvField>() ?: continue
             val headerName = csvField.name.ifBlank { prop.name }
             val index = csvField.index
-            val cell = createCell(headerName, prop)
+            val cell = createAdapter(headerName, prop)
 
             if (0 < index) {
                 if (headerIndexMap.containsKey(index)) {
@@ -167,110 +149,27 @@ class CsvEntityWriter<T : Any>(
         return free ?: (used.maxOrNull()?.plus(1) ?: 1)
     }
 
-    private fun createCell(
+    private fun <U> createAdapter(
         headerName: String,
-        prop: KProperty1<*, *>,
-    ): Cell {
+        prop: KProperty1<*, U>,
+    ): CsvConverterAdapter<U> {
         val converter = resolveConverter(prop)
-        return Cell(headerName, prop, converter)
+        @Suppress("UNCHECKED_CAST")
+        return CsvConverterAdapter(headerName, prop, converter as CsvConverter<U>)
     }
 
     private fun resolveConverter(property: KProperty1<*, *>): CsvConverter<*> {
-        val fieldFmt = property.findAnnotation<CsvFieldFormat>()
-        val locale = fieldFmt?.locale?.let { Locale.forLanguageTag(it) }
-
-        if (property.returnType.classifier == String::class) {
-            return StringCsvConverter()
-        }
-
-        if (property.returnType.classifier == Boolean::class) {
-            val boolFmt = property.findAnnotation<BooleanCsvField>() ?: BooleanCsvField()
-            return BooleanCsvConverter
-                .Builder()
-                .trueValues(boolFmt.trueValues.toList())
-                .falseValues(boolFmt.falseValues.toList())
-                .caseSensitive(!boolFmt.ignoreCase)
-                .build()
-        }
-
-        var builder = when (property.returnType.classifier) {
-            Int::class -> IntCsvConverter.Builder()
-            Long::class -> LongCsvConverter.Builder()
-            Short::class -> ShortCsvConverter.Builder()
-            Byte::class -> ByteCsvConverter.Builder()
-            Float::class -> FloatCsvConverter.Builder()
-            Double::class -> DoubleCsvConverter.Builder()
-            BigDecimal::class -> BigDecimalCsvConverter.Builder()
-            LocalDate::class -> LocalDateCsvConverter.Builder()
-            LocalDateTime::class -> LocalDateTimeCsvConverter.Builder()
-            else -> {
-                throw CsvUnsupportedTypeException(
-                    entityClass,
-                    property.name,
-                    property.returnType,
-                )
-            }
-        }
-
-        if (locale != null) {
-            builder = builder.locale(locale)
-        }
-        if (fieldFmt != null) {
-            builder = builder.pattern(fieldFmt.pattern)
-        }
-
-        return builder.build()
+        return CsvConverterHelper.resolve(entityClass, property)
     }
 }
 
-private class Cell(
+private class CsvConverterAdapter<T>(
     val headerName: String,
-    val prop: KProperty1<*, *>,
-    private val converter: CsvConverter<*>,
+    val prop: KProperty1<*, T>,
+    private val converter: CsvConverter<T>,
 ) {
-    @Suppress("UNCHECKED_CAST")
-    fun serialize(value: Any?): Result<String?> =
-        when (converter) {
-            is IntCsvConverter ->
-                (converter as CsvConverter<Int>).serialize(value as Int?)
-
-            is LongCsvConverter ->
-                (converter as CsvConverter<Long>).serialize(value as Long?)
-
-            is ShortCsvConverter ->
-                (converter as CsvConverter<Short>).serialize(value as Short?)
-
-            is ByteCsvConverter ->
-                (converter as CsvConverter<Byte>).serialize(value as Byte?)
-
-            is FloatCsvConverter ->
-                (converter as CsvConverter<Float>).serialize(value as Float?)
-
-            is DoubleCsvConverter ->
-                (converter as CsvConverter<Double>).serialize(value as Double?)
-
-            is StringCsvConverter ->
-                (converter as CsvConverter<String>).serialize(value as String?)
-
-            is BigDecimalCsvConverter ->
-                (converter as CsvConverter<BigDecimal>).serialize(value as BigDecimal?)
-
-            is LocalDateCsvConverter ->
-                (converter as CsvConverter<LocalDate>).serialize(value as LocalDate?)
-
-            is LocalDateTimeCsvConverter ->
-                (converter as CsvConverter<LocalDateTime>).serialize(value as LocalDateTime?)
-
-            is BooleanCsvConverter ->
-                (converter as CsvConverter<Boolean>).serialize(value as Boolean?)
-
-            else ->
-                Result.failure(
-                    CsvUnsupportedTypeException(
-                        entityClass = prop.returnType.classifier as? KClass<*> ?: Any::class,
-                        paramName = prop.name,
-                        type = prop.returnType,
-                    ),
-                )
-        }
+    fun serialize(value: Any?): Result<String?> {
+        @Suppress("UNCHECKED_CAST")
+        return converter.serialize(value as T?)
+    }
 }
